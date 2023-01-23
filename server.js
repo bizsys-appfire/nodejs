22101b16e76f2b8a35787e9334fa1ced31115f48
root <root@ip-172-31-33-25.us-east-2.compute.internal>
2023-01-23 11:53:20 +0000

    Applied changes in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! thank you very much for creating this page. Now url should show up and running');
+  res.send('Hello World! thank you very much for creating this page. Now url should show up and running.Trying a new attepmt to view the trigger');
 });
 
 app.listen(PORT, HOST, () => {
